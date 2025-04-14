4d7c325214d2c607a5e9515a4f0aec71149aac69
Carlos <carlos97181034535@gmail.com>
2025-04-14 23:47:42 +0200

    added: radio btns selected and assigned a function

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,21 @@
-import { valueToObject, submitForm, generateId } from './functions.js';
+import { readTaskValue, submitForm, generateId, readTaskPriorityValue } from './functions.js';
 
 window.addEventListener('load', () => form.reset());
 
+//attribute 'checked' doesn't work on firefox, need to force the state with js
+document.querySelector('#low-priority').checked = true;
 export const inputTask = document.querySelector('#input-task');
 export const btnAddTasks = document.querySelector('#btn-add-tasks');
 export const form = document.querySelector('#form');
 export const divUserTasks = document.querySelector('#div-user-tasks');
+const radioButtons = document.querySelectorAll('input[type="radio"]');
 
-document.addEventListener('DOMContentLoaded', () => {
-  inputTask.addEventListener('input', valueToObject);
-  form.addEventListener('submit', submitForm);
-});
+inputTask.addEventListener('input', readTaskValue);
+form.addEventListener('submit', submitForm);
+radioButtons.forEach(radioBtn => radioBtn.addEventListener('change', readTaskPriorityValue));
 
 export const tasksObj = {
   id: generateId(),
-  task: ''
-};
+  task: '',
+  priority: 'low'
+}
